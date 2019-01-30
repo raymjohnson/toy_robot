ef29c71580906217774eff474c15150d5c291e7e
jody vanden <jody.vanden@gmail.com>
2019-01-30 16:00:47 +1100

    modify the script to read

--- spec/04_read_file_spec.rb
+++ spec/04_read_file_spec.rb
@@ -3,10 +3,11 @@ RSpec.describe ToyRobot::Operator do
   describe 'reading the file' do
     it 'sends each command' do
       file = 'command.txt'
-      binding.pry
-      File.open(file, 'r') do |line|
-        line.first.split("\n")
-        # ToyRobot::Operator.handle_command(line)
+      File.readlines(file).map do |commands|
+        commands.split.map do |command|
+          command
+        end
+        binding.pry
       end
     end
   end
